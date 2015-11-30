d3a5950dc1436a4adca671d5527161ca63d94332
Bienten <bienten@mail.ru>
2015-11-30 16:59:41 +0300

    Create test.php

---
+++ view/test.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo $title ?></title>
+</head>
+<body>
+    <h1><?php echo $title ?></h1>
+    Category: <?php echo $category ?> <br/>
+    This page: <?php echo $page ?>
+</body>
+</html>
