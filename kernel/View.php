6e48fe412dcd63a15e88c35b7cda843bd440a84b
Bienten <bienten@mail.ru>
2015-11-30 16:59:09 +0300

    Create View.php

---
+++ kernel/View.php
@@ -0,0 +1,31 @@
+<?php
+
+class View {
+
+    private $data = array();
+    private $path = './view/';
+
+    public function get($name) {
+        return $this->data[$name];
+    }
+
+    public function set($name, $value) {
+        $this->data[$name] = $value;
+    }
+
+    public function render($name, $data = array()) {
+
+        if (!empty($data)) {
+            foreach ($data as $k => $v) {
+                $this->set($k, $v);
+            }
+            extract($this->data);
+        }
+
+        if (file_exists($this->path . $name)) {
+            include $this->path . $name;
+        } else die ('Template not found!');
+
+    }
+
+}
